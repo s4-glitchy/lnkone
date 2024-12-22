683604b13c275f953e87e6bf8ad9522ce545cc05
s4-glitchy <s4alicreator001@gmail.com>
2024-12-23 01:11:33 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://lamaretail.com/collections/man-shorts?";
-    const blackPageURL = "https://IFRXJwhbvB.myfunnelish.com/hoodie";
+    const blackPageURL = "https://saizjdkeo.myfunnelish.com/bear-hood";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
